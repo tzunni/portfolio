9eefbedf5b95d3612fda0c2dac64441eda889345
Keith Bui <kqbui@csu.fullerton.edu>
2025-09-10 06:47:03 -0700

    mostly finalized design, filled in all content areas but haven't elaborated on projects yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
+import { AiOutlineSound, AiFillSound } from "react-icons/ai";
+
 
 // Your font imports
 import '@fontsource/roboto';
@@ -8,11 +10,25 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/400-italic.css';
 
 // Placeholder components for your content
-const OverviewContent = () => <h3>This is the Overview page content.</h3>;
+const OverviewContent = () => 
+  <>
+    <h3>Hi, my name is Keith Bui and I'm interested in full-stack development, systems engineering, and IT roles.</h3>
+    <h3>My work experience includes being a Cybersecurity Mentor for Cypress College and working the IT helpdesk at California State University, Fullerton.</h3>
+    <h3>I love to help the community that raised me, whether it's by joining organizations that give back or by helping those around me do better.</h3>
+    <h3>My hobbies include playing games, travelling the world, cooking, and listening to music.</h3>
+    <h3>My creativity and my tenacity fuel my passion for computer science, and I hope that is reflected in this portfolio here.</h3>
+  </>;
 const SkillsContent = () => <h3>This is the Skills page.</h3>;
-const SubSkills1 = () => <h3>This is the content for Sub-skill 1.</h3>;
-const SubSkills2 = () => <h3>This is the content for Sub-skill 2.</h3>;
-const ProjectsContent = () => <h3>This is the Projects page.</h3>;
+const Languages = () => <h3>I've used Python, C/C++, SQL, JavaScript, HTML/CSS, Bash, Swift, but am most competent in Python and front-end development.</h3>;
+const Frameworks = () => <h3>I've used ReactJS, Expo, React Native, Node.js, Flask, and Django, but am most confident in both Reacts and Flask.</h3>;
+const Coursework = () => <h3>I've taken...<br/><br/>Cybersecurity Fundamentals, Cisco Networking 1, Network Security, Ethical Hacking, Object Oriented Programming,
+Algorithm Engineering, Data Structures, Software Engineering, Artificial Intelligence, File Structures and Databases,
+Web Back-End Engineering, iOS Mobile Dev. Programming, Intro. to Data Sci. and Big Data, Computer Communications (in progress)</h3>;
+const ProjectsContent = () => <h3>This is the Projects page. It is still WIP.</h3>;
+const TaskSocial = () => <h3>TaskSocial is my senior capstone project being built in React Native and Go, and is a social media app that is meant to gamify, share, and encourage physical activity.</h3>;
+const TypeFighter = () => <h3>TypeFighter is a joint project with Patrick Tang built in Flask and ReactJS, and is a typing test designed around book prompts. It features a boss and a leaderboard to encourage players to play with and against each other.</h3>;
+const ApexRPC = () => <h3>ApexRPC is an open source Discord Rich Presence Client that broadcasts the Apex Legends in game status onto Discord. It is created around reverse engineering the Steam API, and usees NodeJS to fetch and broadcast.</h3>;
+const BlueTeamCTF = () => <h3>This Blue Team CTF used file analysis, tracking suites, and web vulnerabilities to find flags in a team competition, and we placed 13th/354 teams.</h3>;
 const WorkExpContent = () => <h3>This is the Work Experience page.</h3>;
 const CEDA = () => 
   <>
@@ -28,35 +44,65 @@ const Cypress = () =>
     <h3>I fielded questions about Linux, Windows, Cisco, and SysAdmin configurations, researching them to further my students' understanding of the subject.</h3>
     <h3>I created three lab walkthroughs and performed several in-class demonstrations to guide students during their classwork.</h3>
   </>;
-const Certifications = () => <h3>There are my certications.</h3>;
+const CertificationsContent = () => <h3>These are my certications.</h3>;
 const ITF = () => <h3>This is the CompTIA IT Fundamentals+ certificate. I got this in June 2021.</h3>;
 const CE = () => <h3>This is the CompTIA Cloud Essentials certificate. I got this in June 2021.</h3>;
 const Network = () => <h3>This is the CompTIA Network+ certificate. I got this in November 2021, and it expired November 2024. This was the N10-008 iteration of the exam.</h3>;
+const ClubsContent = () => <h3>These are the clubs and extracurriculars I joined in college.</h3>;
+const UVSA = () => 
+  <>
+    <h3 style={{fontWeight: 'bold'}}>IT Coordinator<br/><span style={{fontWeight: '300', fontStyle: 'italic'}}>Union of Vietnamese Student Associations</span><br/><span style={{fontWeight: '300'}}>Garden Grove, CA</span><br/><span style={{fontWeight: '300'}}>February 2024 – April 2024</span></h3>
+    <h3>I assisted the media team during external events.</h3>
+    <h3>I helped manage and redirect emails during TetFest, one of the organization's busiest times.</h3>
+    <h3>I generated ideas during meetings to help the organization reach more people.</h3>
+  </>;
+const CSUFVSA = () =>
+  <>
+    <h3 style={{fontWeight: 'bold'}}>Intern<br/><span style={{fontWeight: '300', fontStyle: 'italic'}}>Vietnamese Student Association, California State Univerity, Fullerton</span><br/><span style={{fontWeight: '300'}}>Fullerton, CA</span><br/><span style={{fontWeight: '300'}}>October 2022 – June 2023</span></h3>
+    <h3>I built my extroversion from cohosting events and networking with students from our student body, as well as from other schools.</h3>
+    <h3>I helped to mediate conflict amongst the club members and managed crowds during events.</h3>
+    <h3>I encouraged others to reach out and created a safe space for members to celebrate their culture and heritage.</h3>
+  </>;
+const C4 = () => 
+  <>
+    <h3 style={{fontWeight: 'bold'}}>Social Media Manager<br/><span style={{fontWeight: '300', fontStyle: 'italic'}}>Cypress College Cyber Club, Cypress College</span><br/><span style={{fontWeight: '300'}}>Cypress, CA</span><br/><span style={{fontWeight: '300'}}>September 2021 – June 2022</span></h3>
+    <h3>I created fliers every other week for events that the club hosted, such as movie nights, speaker events, and Capture the Flag competitions.</h3>
+    <h3>I collaborated with my club board to create announcements, combat spam, and moderate the discord server.</h3>
+    <h3>I helped come up with and cohosted events online during the pandemic on Zoom.</h3>
+  </>;
+const ContactContent = () => <h3>Here are my links and contact information.</h3>
 const Github = () => <h3>This is my Github link.</h3>;
 const LinkedIn = () => <h3>This is my LinkedIn link.</h3>;
 const Resume = () => <h3>This is my Resume link.</h3>;
+const Email = () => <h3>This is my email, keithqbui@gmail.com.</h3>
 
 function App() {
   const backSound = new Audio('/back_sound.mp3'); 
-  const clickSound = new Audio('/click_sound.mp3'); 
+  const clickSound = new Audio('/click_sound.mp3');
+  const hoverSound = new Audio('/hover_sound.mp3');
   const menuData = {
     main: [
-      // { id: 'overview', title: 'Overview', content: <OverviewContent /> },
       { id: 'overview', title: 'Overview', content: <OverviewContent /> },
       { id: 'skills', title: 'Skills', content: <SkillsContent/> },
       { id: 'projects', title: 'Projects', content: <ProjectsContent/>},
       { id: 'work experience', title: 'Work Experience', content: <WorkExpContent/>},
-      { id: 'certifications', title: 'Certifications', content: <Certifications/>},
-      { id: 'clubs', title: 'Clubs'},
-      { id: 'contact', title: 'Links/Contact'},
+      { id: 'certifications', title: 'Certifications', content: <CertificationsContent/>},
+      { id: 'clubs', title: 'Clubs', content: <ClubsContent/>},
+      { id: 'contact', title: 'Links/Contact', content: <ContactContent/>},
     ],
     subMenus: {
       'overview': [],
       'skills': [
-        { id: 'subskill1', title: 'Sub-skill 1', content: <SubSkills1/>},
-        { id: 'subskill2', title: 'Sub-skill 2', content: <SubSkills2/>},
+        { id: 'languages', title: 'Languages', content: <Languages/>},
+        { id: 'frameworks/tools', title: 'Frameworks and Tools', content: <Frameworks/>},
+        { id: 'coursework', title: 'Coursework', content: <Coursework/>},
       ],
-      'projects': [],
+      'projects': [
+        { id: 'tasksocial', title: 'TaskSocial', content: <TaskSocial/>},
+        { id: 'typefighter', title: 'TypeFighter', content: <TypeFighter/>},
+        { id: 'apexrpc', title: 'ApexRPC', content: <ApexRPC/>},
+        { id: 'btctf', title: 'Blue Team CTF', content: <BlueTeamCTF/>},
+      ],
       'work experience': [
         { id: 'ceda', title: 'IT Student Assistant', content: <CEDA/>},
         { id: 'cypress', title: 'Cybersecurity Mentor', content: <Cypress/>},
@@ -66,11 +112,16 @@ function App() {
         { id: 'ce', title: 'Cloud Essentials', content: <CE/>, link: "https://www.certmetrics.com/comptia/public/verification.aspx?code=KKTWQ4S7Z4P6V69J"},
         { id: 'network', title: 'Network+', content: <Network/>, link: "https://www.certmetrics.com/comptia/public/verification.aspx?code=9ET0292F834EQQG2"},
       ],
-      'clubs': [],
+      'clubs': [
+        { id: 'UVSA', title: 'UVSA', content: <UVSA/>},
+        { id: 'CSUFVSA', title: 'CSUF VSA', content: <CSUFVSA/>},
+        { id: 'C4', title: 'C4', content: <C4/>},
+      ],
       'contact': [
         {id: 'github', title: 'Github', link: "https://github.com/tzunni", content: <Github/>},
         {id: 'linkedin', title: 'LinkedIn', link: "https://www.linkedin.com/in/keithqbui/", content: <LinkedIn/>},
-        {id: 'resume', title: 'Resume', link: "/resume.pdf", content: <Resume/>}
+        {id: 'resume', title: 'Resume', link: "/resume.pdf", content: <Resume/>},
+        {id: 'email', title: 'Email', link: "mailto:keithqbui@gmail.com", content: <Email/>}
       ],
     },
   };
@@ -83,6 +134,7 @@ function App() {
   const [isMenuHidden, setIsMenuHidden] = useState(false);
   const [isContentHidden, setIsContentHidden] = useState(true);
   const [isPlayerHidden, setIsPlayerHidden] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
   const menuRef = useRef(null);
   const cursorRef = useRef(null);
 
@@ -129,7 +181,12 @@ function App() {
   }, [activeLink]); 
 
   const handleMouseEnter = (linkId) => {
+    if (!isMuted) {hoverSound.play();};
     setActiveLink(linkId);
+  };
+
+  const toggleMute = () => {
+    setIsMuted(!isMuted);
   };
 
   const handleMenuClick = (linkId) => {
@@ -138,7 +195,7 @@ function App() {
     const item = currentMenu.find(item => item.id === linkId);
     if (linkId === 'back') {
       setTimeout(() => {
-        backSound.play(); // Play the sound immediately
+        if (!isMuted) {backSound.play();};
         setIsMenuHidden(false);
         setIsMainMenu(true);
         setActiveLink('overview');
@@ -148,13 +205,13 @@ function App() {
       }, 250);
     } else if (item.link) {
       setTimeout(() => {
-        clickSound.play();
+        if (!isMuted) {clickSound.play();};
         setIsMenuHidden(false);
         window.open(item.link, '_blank');
       }, 250);
     } else if (menuData.subMenus[linkId]) {
       setTimeout(() => {
-        clickSound.play(); // Play the sound immediately
+        if (!isMuted) {clickSound.play();};
         setIsMenuHidden(false);
         setIsMainMenu(false);
         setCurrentSubMenu(linkId);
@@ -165,7 +222,7 @@ function App() {
       }, 250);
     } else {
       setTimeout(() => {
-        clickSound.play(); // Play the sound immediately
+        if (!isMuted) {clickSound.play();};
         setIsMenuHidden(false);
         setCurrentParentMenu(activeLink);
         setActiveLink('back');
@@ -258,6 +315,9 @@ function App() {
       </div>
       <div id="content-container" className={isContentHidden ? 'hidden' : ''}>
         {renderContent()}
+      </div>
+      <div onClick={toggleMute}>
+        {isMuted ? <AiOutlineSound id='mute'/> : <AiFillSound id='mute'/>}
       </div>
     </div>
   );
